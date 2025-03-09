a7a2050c8b14a76ed2b7425f80eeec001d2922ee
Haseeburrahmann <mohd.haseeb.4201@gmail.com>
2025-03-09 16:01:50 -0500

    commiting

--- src/app/features/users/user-management/user-management.routes.ts
+++ src/app/features/users/user-management/user-management.routes.ts
@@ -1,30 +1,30 @@
-// // src/app/features/users/user-management/user-management.routes.ts
-// import { Routes } from '@angular/router';
-// import { AuthGuard } from '../../../core/auth/auth.guard';
+// src/app/features/users/user-management/user-management.routes.ts
+import { Routes } from '@angular/router';
+import { AuthGuard } from '../../../core/auth/auth.guard';
 
-// export const USER_MANAGEMENT_ROUTES: Routes = [
-//   {
-//     path: '',
-//     loadComponent: () => import('./user-list/user-list.component').then(m => m.UserListComponent),
-//     canActivate: [AuthGuard],
-//     data: { requiredPermission: 'users:read' }
-//   },
-//   {
-//     path: 'create',
-//     loadComponent: () => import('./user-form/user-form.component').then(m => m.UserFormComponent),
-//     canActivate: [AuthGuard],
-//     data: { requiredPermission: 'users:write' }
-//   },
-//   {
-//     path: ':id',
-//     loadComponent: () => import('./user-detail/user-detail.component').then(m => m.UserDetailComponent),
-//     canActivate: [AuthGuard],
-//     data: { requiredPermission: 'users:read' }
-//   },
-//   {
-//     path: ':id/edit',
-//     loadComponent: () => import('./user-form/user-form.component').then(m => m.UserFormComponent),
-//     canActivate: [AuthGuard],
-//     data: { requiredPermission: 'users:write', isEdit: true }
-//   }
-// ];
+export const USER_MANAGEMENT_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./user-list/user-list.component').then(m => m.UserListComponent),
+    canActivate: [AuthGuard],
+    data: { requiredPermission: 'users:read' }
+  },
+  {
+    path: 'create',
+    loadComponent: () => import('./user-form/user-form.component').then(m => m.UserFormComponent),
+    canActivate: [AuthGuard],
+    data: { requiredPermission: 'users:write' }
+  },
+  {
+    path: ':id',
+    loadComponent: () => import('./user-detail/user-detail.component').then(m => m.UserDetailComponent),
+    canActivate: [AuthGuard],
+    data: { requiredPermission: 'users:read' }
+  },
+  {
+    path: ':id/edit',
+    loadComponent: () => import('./user-form/user-form.component').then(m => m.UserFormComponent),
+    canActivate: [AuthGuard],
+    data: { requiredPermission: 'users:write', isEdit: true }
+  }
+];
--- src/app/layouts/main-layout/sidebar/sidebar.component.ts
+++ src/app/layouts/main-layout/sidebar/sidebar.component.ts
@@ -162,16 +162,16 @@ export class SidebarComponent implements OnInit {
             icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>'
           },
           {
-            label: 'Users',
-            route: '/users',
+            label: 'User List',
+            route: '/users/management',
             icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" /></svg>',
             permission: 'users:read'
           },
           {
-            label: 'Roles',
-            route: '/roles',
-            icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" /></svg>',
-            permission: 'roles:read'
+            label: 'Add New User',
+            route: '/users/management/create',
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" /></svg>',
+            permission: 'users:write'
           }
         ]
       },
@@ -243,7 +243,7 @@ export class SidebarComponent implements OnInit {
           }
         ]
       }
-    ];
+    ]
     
     // Filter sections based on permissions
     this.navSections = allSections
